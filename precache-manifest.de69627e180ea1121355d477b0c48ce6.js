4b548e3d4fdcabffce4b79c1ce47b083d99575d7
Stas Mikhailov <mik.stanis@yandex.ru>
2020-08-11 21:45:04 +0300

    Updates

---
+++ precache-manifest.de69627e180ea1121355d477b0c48ce6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0407f45ae028be73911f811c090db0",
+    "url": "/Haugety/mesto-react/index.html"
+  },
+  {
+    "revision": "5b7d193815aa93aa8f20",
+    "url": "/Haugety/mesto-react/static/css/main.40d8aa02.chunk.css"
+  },
+  {
+    "revision": "fd689bf95fcdc6f87d88",
+    "url": "/Haugety/mesto-react/static/js/2.8b66efd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Haugety/mesto-react/static/js/2.8b66efd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7d193815aa93aa8f20",
+    "url": "/Haugety/mesto-react/static/js/main.88a04766.chunk.js"
+  },
+  {
+    "revision": "790342dd17b061be55e9",
+    "url": "/Haugety/mesto-react/static/js/runtime-main.198bfefc.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/Haugety/mesto-react/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/Haugety/mesto-react/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/Haugety/mesto-react/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/Haugety/mesto-react/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/Haugety/mesto-react/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/Haugety/mesto-react/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/Haugety/mesto-react/static/media/activatedLike.5bbb05f7.svg"
+  },
+  {
+    "revision": "055023bb31ce22a900ab927d03a48bc4",
+    "url": "/Haugety/mesto-react/static/media/close-icon.055023bb.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/Haugety/mesto-react/static/media/deactivatedLike.a8acc871.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/Haugety/mesto-react/static/media/mesto-logo.c6f11019.svg"
+  },
+  {
+    "revision": "4220563aa7e7f6bb1d562a95174678b6",
+    "url": "/Haugety/mesto-react/static/media/trash-button.4220563a.svg"
+  }
+]);
