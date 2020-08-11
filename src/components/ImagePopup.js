0b5cd71dd6c14bcfa79abbf9b56fcad37703a5a5
Stas Mikhailov <mik.stanis@yandex.ru>
2020-08-11 21:35:08 +0300

    fix: project 10

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,7 +5,7 @@ function ImagePopup({ isOpen, selectedCardLink, selectedCardName, onClose }) {
 
     <section className={`popup ${isOpen ? "popup_opened" : ""}`}>
       <figure className="popup__image-container">
-        <img src={ selectedCardLink } className="popup__image" />
+        <img alt={ selectedCardName } src={ selectedCardLink } className="popup__image" />
         <figcaption className="popup__caption">{ selectedCardName }</figcaption>
         <button type="button" className="popup__close-button" onClick={ onClose }></button>
       </figure>
